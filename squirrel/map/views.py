cfb0415665f904864f9139383430349d599b553f
siyu <sy2882@columbia.edu>
2019-12-09 02:07:12 +0000

    guess nt here

--- squirrel/map/views.py
+++ squirrel/map/views.py
@@ -6,7 +6,7 @@ from .models import squirrels
 
 
 def squirrels_map(request):
-    sightings=squirrels.objects.all()[:50]
+    sightings=squirrels.objects.all()[:100]
     
     return render(request, 'map/squirrels_map.html',{'sightings':sightings})
 # Create your views here.
