fdfeb7f1614a03b64a8d5cc5b63a54ddd434845d
yuhui19970814 <58612948+yuhui19970814@users.noreply.github.com>
2019-12-10 05:29:43 +0800

    Update views.py

--- squirrel/sightings/views.py
+++ squirrel/sightings/views.py
@@ -1,69 +1,44 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.http import HttpResponse
-from .models import Squirrel
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from map.models import squirrels
 from .forms import SquirrelForm
 
-def all_squirrels(request):
-    squirrels=Squirrel.objects.all()[:45]
-    context = {
-        'squirrels':squirrels,
-    }
-    return render(request, 'sightings/all.html',context)
-
-def update_squirrels(request, unique_squirrel_id):
-     squirrel=Squirrel.objects.get(id=unique_squirrel_id)
-     if request.method == 'POST':
-         form=SquirrelForm(request.POST, instance=squirrel)
-         
-         if form.is_valid():
-             form.save()
-             return redirect(f'/sightings/{unique_squirrel_id}')
-     else:
-         form=SquirrelForm(instance=squirrel)
-     context = {
-         'form':form,
-     }
-     return render(request, 'sightings/update.html',context)
-
-def add_squirrels(request):
-    if request.method =='POST':
-        form = SquirrelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(f'/sightings/')
-    else:
-        form=SquirrelForm()
-    context ={
-        'form':form,
-    }
-    return render(request, 'sightings/update.html',context)
+def index(request):
+    sightings=squirrels.objects.all()
+    context={'sightings':sightings,} 
+    return render(request,"sightings/index.html",context)
 
 def stats(request):
-    total_number = Squirrel.objects.all().count()
-    adult = Squirrel.objects.filter(age='Adult').count()
-    juvenile = Squirrel.objects.filter(age='Juvenile').count()
-    black = Squirrel.objects.filter(color='Black').count()
-    cinnamon = Squirrel.objects.filter(color='Cinnamon').count()
-    gray = Squirrel.objects.filter(color='Gray').count()
-    running = Squirrel.objects.filter(running=True).count()
-    chasing = Squirrel.objects.filter(chasing=True).count()
-    climbing = Squirrel.objects.filter(climbing=True).count()
-    eating = Squirrel.objects.filter(eating=True).count()
-    foraging = Squirrel.objects.filter(foraging=True).count()
-    context = {
-        'total_numbel': total_number,
-        'adult': adult,
-        'juvenile': juvenile,
-        'black': black,
-        'cinnamon': cinnamon,
-        'gray': gray,
-        'running': running,
-        'chasing':chasing,
-        'climbing':climbing,
-        'eating':eating,
-        'foraging':foraging,
-    }
-    return render(request, 'sightings/stats.html', context)
+    stats1=squirrels.objects.all().count()
+    stats2=squirrels.objects.filter(Runs_from=True).count()
+    stats3=squirrels.objects.filter(Climbing=True).count()
+    stats4=squirrels.objects.filter(Primary_fur_color='Black').count()
+    stats5=squirrels.objects.filter(Chasing=True).count()
+    context={'stats1':stats1,'stats2':stats2,'stats3':stats3,'stats4':stats4,'stats5':stats5,}
+    return render(request,'sightings/stats.html',context)
 
+def add(request):
+    if request.method=="POST":
+        form=SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sightings:index')
+    else:
+        form=SquirrelForm()
+    context={'form':form}
+    return render(request,'sightings/add.html',context)
+
+
+def update(request, Unique_Squirrel_ID):
+# updating user information
+    sighting = squirrels.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
+    if request.method=="POST":
+        form=SquirrelForm(request.POST,instance=sighting)
+        if form.is_valid():
+            form.save()
+            return redirect('sightings:index')
+    else:
+        form=SquirrelForm(instance=sighting)
+    context={'form':form}
+    return render(request,'sightings/update.html',context)
 
