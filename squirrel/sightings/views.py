ac22de6f9b03049f56431e508456afabc34763f3
yuhui19970814 <58612948+yuhui19970814@users.noreply.github.com>
2019-12-10 02:50:44 +0800

    Update views.py

--- squirrel/sightings/views.py
+++ squirrel/sightings/views.py
@@ -3,27 +3,30 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Squirrel
 from .forms import SquirrelForm
+
 def all_squirrels(request):
     squirrels=Squirrel.objects.all()[:100]
     context = {
         'squirrels':squirrels,
     }
     return render(request, 'sightings/all.html',context)
+
 def update_squirrels(request, unique_squirrel_id):
      squirrel=Squirrel.objects.get(id=unique_squirrel_id)
      if request.method == 'POST':
          form=SquirrelForm(request.POST, instance=squirrel)
-         # correct????
+         
          if form.is_valid():
              form.save()
-             return redirect(f'/sighting/{unique_squirrel_id}')
+             return redirect(f'/sightings/{unique_squirrel_id}')
      else:
          form=SquirrelForm(instance=squirrel)
      context = {
          'form':form,
      }
      return render(request, 'sightings/update.html',context)
-def add_squirrels(request):
+
+    def add_squirrels(request):
     if request.method =='POST':
         form = SquirrelForm(request.POST)
         if form.is_valid():
