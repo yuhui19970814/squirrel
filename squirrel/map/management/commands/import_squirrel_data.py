3d4870211baefa40b6374f1eb5dbfcffb7fd034c
sisi28888 <57200801+sisi28888@users.noreply.github.com>
2019-12-09 19:30:43 -0500

    Update import_squirrel_data.py
    change 3

--- squirrel/map/management/commands/import_squirrel_data.py
+++ squirrel/map/management/commands/import_squirrel_data.py
@@ -3,7 +3,7 @@ from map.models import squirrels
 import csv
 from django.core.management import BaseCommand
 
-class Command(BaseCommand)
+class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('csv_file')
